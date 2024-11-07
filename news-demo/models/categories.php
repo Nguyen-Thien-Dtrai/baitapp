a619a841deed3066fb5941b2b83c8ed89ad36c6c
Nguyen-Thien-Dtrai <23211TT4032@mail.tdc.edu.vn>
2024-11-07 11:00:51 +0700

    first t

--- news-demo/models/categories.php
+++ news-demo/models/categories.php
@@ -1,11 +1,22 @@
 <?php
-class Category extends db{
-    public function getAllCate(){
-        $sql = self:: $connection -> prepare("SELECT * FROM categories");
-        $sql -> execute();
+class Category extends db
+{
+    public function getAllCate()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM categories");
+        $sql->execute();
         $items = array();
-        $items = $sql->get_result()-> fetch_all(MYSQLI_ASSOC);
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items;
+    }
+    public function getCateNameByID($cate_id)
+    {
+        $sql = self::$connection->prepare("SELECT *FROM categroies WHERE `id` = ?");
+        $sql->bind_param("i", $cate_id);
+        $sql->execute();
+        $items = array();
+
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items;
     }
 }
-?>
--- news-demo/models/items.php
+++ news-demo/models/items.php
@@ -1,11 +1,21 @@
 <?php
-class items extends Db{
-    public function getAllItems(){
-        $sql = self :: $connection -> prepare("SELECT *FROM items");
-        $sql ->execute();
+class items extends Db
+{
+    public function getAllItems()
+    {
+        $sql = self::$connection->prepare("SELECT *FROM items");
+        $sql->execute();
         $items = array();
-        $items = $sql -> get_result()-> fetch_all(MYSQLI_ASSOC);
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items;
+    }
+    public function getFeaturedItem()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM `items`
+        WHERE `featured` = 1 ORDER BY `created_at` DESC LIMIT ?.?");
+        $sql->execute();
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items;
     }
 }
-?>
